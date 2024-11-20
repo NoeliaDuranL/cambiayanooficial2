0ca212bb4c658343be1dfc545c9815f3551496e0
Michael-Flores <micha11.mrfv@gmail.com>
2024-11-20 07:15:58 -0400

    agregar productos

---
+++ app/src/main/java/com/example/cambiayanooficial2/models/request/Product.kt
@@ -0,0 +1,7 @@
+package com.example.cambiayanooficial2.models.request
+
+data class Product(
+    val nombre: String,
+    val descripcion: String,
+    val imagen: String,
+)
--- app/src/main/java/com/example/cambiayanooficial2/network/ApiService.kt
+++ app/src/main/java/com/example/cambiayanooficial2/network/ApiService.kt
@@ -3,13 +3,19 @@ package com.example.cambiayanooficial2.network
 import com.example.cambiayanooficial2.models.PostProductoRequest
 import com.example.cambiayanooficial2.models.Publicacion
 import com.example.cambiayanooficial2.models.request.LoginRequest
+import com.example.cambiayanooficial2.models.request.Product
 import com.example.cambiayanooficial2.models.request.RegisterRequest
 import com.example.cambiayanooficial2.models.response.ApiResponse
+import com.example.cambiayanooficial2.models.response.ImageUploadResponse
+import com.example.cambiayanooficial2.models.response.ProductResponse
 import com.example.cambiayanooficial2.models.response.RegisterResponse
 import com.example.cambiayanooficial2.models.response.UploadResponse
+import com.example.cambiayanooficial2.models.response.responseProducto
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
@@ -32,11 +38,15 @@ interface ApiService {
     @POST("post-producto")
     suspend fun crearProductoYPost(@Body producto: PostProductoRequest): Publicacion
 
+    // Endpoint para crear un producto
+    @POST("products")
+    suspend fun createProduct(@Body product: Product): Response<ProductResponse>
+
     // Endpoint para subir la imagen
     @Multipart
-    @POST("upload-image")
+    @POST("products/uploadImage")
     fun uploadImage(
         @Part image: MultipartBody.Part,
-        @Part("id_producto") idProducto: RequestBody
-    ): Call<UploadResponse>
+        @Part("productId") productId: RequestBody
+    ): Call<ImageUploadResponse>
 }
